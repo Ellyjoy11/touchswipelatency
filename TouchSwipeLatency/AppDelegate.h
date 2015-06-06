11ff1596f2dee1fb71803c9f291b8fd2291cb3be
Ellyjoy11 <ellyjoy11@gmail.com>
2015-06-06 09:04:26 -0500

    Initial Commit

---
+++ TouchSwipeLatency/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchSwipeLatency
+//
+//  Created by Elena Last on 6/6/15.
+//  Copyright (c) 2015 Elena Last. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchSwipeLatency/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchSwipeLatency
+//
+//  Created by Elena Last on 6/6/15.
+//  Copyright (c) 2015 Elena Last. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
